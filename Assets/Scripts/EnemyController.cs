50982b379052d81b54437b5158838c4384b1a086
Willg0115 <willg0115@outlook.com>
2025-03-19 19:52:09 -0400

    level 2 started, basic patrol enemies created and implemented

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+        }else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;    
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;    
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.2f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.2f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
