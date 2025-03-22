1bbf6efd1a80d1c74327a7f2624ae5bb8fc9596f
Alrick VINCENT <alrick.vincent@gmail.com>
2025-03-21 23:08:15 -0700

    Added Player Idle & Running Animations/particles
    Added png assets for the player staying idle and not running, which are triggered when the player's horizontal move is non zero. The sprite is simply flipped and the particles switch local position around the player (not super clean)

---
+++ Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AnimationScript : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem runDust;
+
+    private Animator anim;
+    private Player playerMove;
+    private Collision coll;
+    private SpriteRenderer sprite;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMove = GetComponentInParent<Player>();
+        coll = GetComponentInParent<Collision>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("isGrounded", coll.isGrounded);
+        anim.SetBool("isWalled", coll.isWalled);
+        anim.SetBool("rightWalled", coll.rightWalled);
+        anim.SetBool("isGrabbing", playerMove.isGrabbing);
+        anim.SetBool("isDashing", playerMove.isDashing);
+        anim.SetBool("isSliding", playerMove.isSliding);
+        anim.SetFloat("horizontalMove", Mathf.Abs(playerMove.horizontalMove));
+        HandleSpriteFlip();
+        runParticle();
+    }
+
+    public void HandleSpriteFlip()
+    {
+        if (playerMove.horizontalMove > 0.01)
+        {
+            sprite.flipX = true;
+            flipRunParticle(true);
+
+
+        }
+        else if (playerMove.horizontalMove < -0.01)
+        {
+            sprite.flipX = false;
+            flipRunParticle(false);
+
+
+        }
+    }
+
+    private void runParticle()
+    {
+        bool isRunning = playerMove.horizontalMove != 0;
+        if (coll.isGrounded && isRunning)
+        {
+            if (!runDust.isEmitting)
+                runDust.Play();
+        }
+        else
+        {
+            if (runDust.isEmitting)
+                runDust.Stop();
+        }
+    }
+
+    private void flipRunParticle(bool lookingRight)
+    {
+        var particleDir = runDust.forceOverLifetime.x;
+        if (lookingRight)
+        {
+            runDust.transform.localPosition = new Vector3(-0.08f, -0.05f, 0);
+        }
+        else
+        {
+            runDust.transform.localPosition = new Vector3(0.08f, -0.05f, 0);
+        }
+    }
+
+
+}
